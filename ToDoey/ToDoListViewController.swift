53bd40d7e9794bcd7826f2d6a2bbe29b371f266a
Evgeniy Uskov <driftrby@gmail.com>
2019-07-08 12:06:30 +0700

    Add TableViewDataSource Add TableViewDelegate methods Add didSelectRow method

---
+++ ToDoey/ToDoListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ToDoey
+//
+//  Created by Evgeniy Uskov on 08/07/2019.
+//  Copyright © 2019 Evgeniy Uskov. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy eggos", "Destroy Demogorogn "]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     }
+
+    // MARK: TableView DataSource methods
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:   "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    // MARK: TableView  Delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
